23ade9581a397028dbdbbd98b43849228d71d0c5
BB-Kang <kanghm0804@gmail.com>
2022-02-23 16:10:28 +0900

    user ver.2

--- user/models.py
+++ user/models.py
@@ -7,10 +7,19 @@ class UserModel(AbstractUser):
     class Meta:
         db_table = "my_user"
 
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=100, editable=True)
     nickname = models.CharField(max_length=30, default='')
 
 
-class UserDrawingModel(models.Model):
-    pass
+# class DrawingModel(models.Model):
+#     pass
+#
+#
+# class UserDrawingModel(models.Model):
+#     class Meta:
+#         db_table = "my_drawing"
+#
+#     user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+#     drawing_id = models.ForeignKey(DrawingModel, on_delete=models.CASCADE)
+
+
+
--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from user.models import UserModel
 
 # Create your views here.
 
+
 def sign_up(request):
     if request.method == 'GET':
         user = request.user.is_authenticated  # 로그인 되어 있는지
@@ -11,7 +12,7 @@ def sign_up(request):
             return redirect('/')  # 로그인 되어있으면 메인페이지로
         else:
             # 그게 아니면 회원가입 페이지 보여주면 된다
-            return render(request, '/sign_up.html')
+            return render(request, 'user/sign_up.html')
 
     if request.method == 'POST':
         username = request.POST.get('email', '')
@@ -20,16 +21,16 @@ def sign_up(request):
         nickname = request.POST.get('nickname', '')
 
         if password != password2:
-            return render(request, '/sign_up.html', {'error': '입력하신 비밀번호가 일치하지 않습니다'})
+            return render(request, 'user/sign_up.html', {'error': '입력하신 비밀번호가 일치하지 않습니다'})
         elif username == '' or password == '' or nickname == '':
-            return render(request, '/sign_up.html', {'error': '빈 칸이 존재합니다'})
+            return render(request, 'user/sign_up.html', {'error': '빈 칸이 존재합니다'})
 
         exist_user = get_user_model().objects.filter(username=username)
         exist_nick = get_user_model().objects.filter(nickname=nickname)
         if exist_user:
-            return render(request, '/sign_up.html', {'error': '이미 가입 완료 된 이메일입니다'})
+            return render(request, 'user/sign_up.html', {'error': '이미 가입 완료 된 이메일입니다'})
         elif exist_nick:
-            return render(request, '/sign_up.html', {'error': '이미 존재 하는 닉네임입니다'})
+            return render(request, 'user/sign_up.html', {'error': '이미 존재 하는 닉네임입니다'})
         else:
             user = UserModel.objects.create_user(
                 username=username,
@@ -39,6 +40,10 @@ def sign_up(request):
             user.is_active = False
             user.save()
             return redirect('/sign_in.html')
+
+
+def sign_in(request):
+    pass
 
 
 
