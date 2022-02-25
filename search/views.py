4f0201b59369f1c23aac4b386e992de320b6ab05
jihoonseo <seojh8910@gmail.com>
2022-02-25 16:58:37 +0900

    search

--- search/views.py
+++ search/views.py
@@ -1,8 +1,29 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Q
 from django.shortcuts import render
 
-# Create your views here.
+from drawing.models import DrawingModel
+
 
 def search(request):
-    if request.method == 'POST':
-        print(request.POST['input'])
-        return render(request, 'search/search.html', {'output': request.POST['input']})
+    search = request.GET.get('search', '')
+    if search:
+        search_list = DrawingModel.objects.filter(Q(author__username__icontains=search) |
+                                                  Q(description__icontains=search))
+
+        # 페이지 나누기
+        page = request.GET.get('page')
+        paginator = Paginator(search_list, 12)
+        rooms = paginator.get_page(page)
+        # if search_list:
+        #     return render(request, 'search/search.html',
+        #                   {'search_list': search_list, 'search': search, 'all': len(search_list)})
+        # else:
+        #     return render(request, 'search/search.html', {'error': search, 'search': search})
+
+        if len(search_list) == 0:
+            return render(request, 'search/search.html', {'error': search, 'search': search})
+        else:
+            return render(request, 'search/search.html',
+                          {'search_list': search_list, 'search': search, 'all': len(search_list), 'page': rooms})
+
