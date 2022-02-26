a4759c2144b07486447f4f095021f6d8f311a850
hong <hcy1997@naver.com>
2022-02-27 03:54:59 +0900

    update - upload css, html add - upload.js

--- static/js/upload.js
+++ static/js/upload.js
@@ -1,18 +1,44 @@
 function loadFile(input) {
     let file = input.files[0];
     let file_img = document.getElementById("input_img");
-    file_img.src = URL.createObjectURL(file);
-    document.getElementById('empty_img').style.display = 'none'
-    document.getElementById('input_img_wrapper').style.display = 'flex'
-    document.getElementById('input_img_wrapper').classList.remove('hidden')
+    let empty_img = document.getElementById('empty_img')
+    if (empty_img.style.display != 'none') {
+        empty_img.classList.add('img_rotate3d')
+        setTimeout(() => {
+            empty_img.style.display = 'none'
+            file_img.src = URL.createObjectURL(file);
+            document.getElementById('input_img_wrapper').style.display = 'flex'
+            document.getElementById('input_img_wrapper').classList.remove('hidden')
+        }, 500)
+    } else {
+        file_img.classList.add('img_rotate3d')
+        setTimeout(() => {
+            file_img.classList.remove('img_rotate3d')
+            file_img.src = URL.createObjectURL(file);
+        }, 500)
+    }
 }
 
 function click_style(input) {
     let style_img = document.getElementById('style_img')
-    style_img.src = input.src
-    document.getElementById('empty_style').style.display = 'none'
-    document.getElementById('style_img_wrapper').style.display = 'flex'
-    document.getElementById('style_img_wrapper').classList.remove('hidden')
+    let empty_style = document.getElementById('empty_style')
+    if (empty_style.style.display != 'none') {
+        empty_style.classList.add('img_rotate3d')
+        setTimeout(() => {
+            empty_style.style.display = 'none'
+            style_img.src = input.src
+            document.getElementById('style_img_wrapper').classList.remove('hidden')
+            document.getElementById('style_img_wrapper').style.display = 'flex'
+        }, 500)
+    } else {
+        style_img.classList.add('img_rotate3d')
+        setTimeout(() => {
+            style_img.classList.remove('img_rotate3d')
+            style_img.src = input.src
+        }, 500)
+    }
+
+
 }
 
 function img_style_change() {
@@ -68,9 +94,31 @@ function posting() {
         }
     });
 }
-function posting1(){
+
+function posting1() {
     document.getElementById('style_box').classList.add('fade-out-box')
     document.getElementById('img_options').classList.add('fade-out-box')
     document.getElementById('input_p').classList.add('fade-out-box')
     document.getElementById('style_p').classList.add('fade-out-box')
+    document.getElementById('input_img').classList.add('small_box')
+    document.getElementById('style_img').classList.add('small_box')
+    setTimeout(() => {
+        document.getElementById('upload_container').style.display = 'none'
+        document.getElementById('move1').src = document.getElementById('input_img').src
+        document.getElementById('move2').src = document.getElementById('style_img').src
+        document.getElementById('move1').style.width = document.getElementById('input_img').style.width
+        document.getElementById('move2').style.width = document.getElementById('style_img').style.width
+        document.getElementById('move1').style.height = document.getElementById('input_img').style.height
+        document.getElementById('move2').style.height = document.getElementById('style_img').style.height
+        document.getElementById('move_div').style.display = 'flex'
+        setTimeout(() => {
+            document.getElementById('move_div').style.display = 'none'
+            document.getElementById('output').classList.remove('hidden')
+            document.getElementById('output').classList.add('fade-in-box')
+            setTimeout(() => {
+                document.getElementById('output_btns').style.display = 'flex'
+                document.getElementById('output_btns').classList.add('fade-in-box1')
+            }, 3000)
+        }, 2000)
+    }, 1200)
 }
