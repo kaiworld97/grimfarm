6bd68c04cbb2df2a5b93245136c5a80b70e71ed2
BB-Kang <kanghm0804@gmail.com>
2022-02-28 22:34:07 +0900

    update validation & alert

--- config/settings.py
+++ config/settings.py
@@ -126,3 +126,4 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "user.UserModel"
+
--- user/models.py
+++ user/models.py
@@ -11,13 +11,7 @@ class UserModel(AbstractUser):
     nickname = models.CharField(max_length=30, default='')
 
 
-# class UserDrawingModel(models.Model):
-#     class Meta:
-#         db_table = "my_drawing"
-#
-#     user_id = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-#     drawing_id = models.ForeignKey(DrawingModel, on_delete=models.CASCADE)
-#     category = models.CharField(max_length=30, default='')
+
 
 
 
